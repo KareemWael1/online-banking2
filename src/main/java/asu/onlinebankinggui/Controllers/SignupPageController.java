4d944237873c6c9fc9c11c0c03cd08f7b704b226
KareemWael1 <87460487+KareemWael1@users.noreply.github.com>
2023-06-02 02:01:14 +0300

    Added signup error handling

--- src/main/java/asu/onlinebankinggui/Controllers/SignupPageController.java
+++ src/main/java/asu/onlinebankinggui/Controllers/SignupPageController.java
@@ -1,6 +1,7 @@
 package asu.onlinebankinggui.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -14,13 +15,82 @@ public class SignupPageController {
     @FXML
     private TextField lastName;
     @FXML
-    private TextField email;
+    private TextField username;
     @FXML
     private PasswordField password;
+    @FXML
+    private Label firstNameError;
+    @FXML
+    private Label lastNameError;
+    @FXML
+    private Label UsernameError;
+    @FXML
+    private Label passwordError;
+
 
     @FXML
     protected void onSignupButtonClick() {
-        // TODO: Call signup() from backend
+        boolean valid = true;
+        String first = firstName.getText();
+        String last = lastName.getText();
+        String userName = username.getText();
+        String pswd = password.getText();
+
+        if(first.isEmpty()){
+            firstNameError.setVisible(true);
+            firstNameError.setText("First Name is empty");
+            valid = false;
+        }
+        else if(!first.matches("[a-zA-Z]+")){
+            firstNameError.setVisible(true);
+            firstNameError.setText("First Name must contain only letters");
+            valid = false;
+        }
+        else {
+            firstNameError.setVisible(false);
+        }
+
+        if(last.isEmpty()){
+            lastNameError.setVisible(true);
+            lastNameError.setText("last Name is empty");
+            valid = false;
+        }
+        else if(!last.matches("[a-zA-Z]+")){
+            lastNameError.setVisible(true);
+            lastNameError.setText("last Name must contain only letters");
+            valid = false;
+        }
+        else {
+            lastNameError.setVisible(false);
+        }
+
+        if(userName.isEmpty()){
+            UsernameError.setVisible(true);
+            UsernameError.setText("Username is empty");
+            valid = false;
+        }
+        else {
+            UsernameError.setVisible(false);
+        }
+
+        if(pswd.isEmpty()){
+            passwordError.setVisible(true);
+            passwordError.setText("Password is empty");
+            valid = false;
+        }
+        else if(pswd.length() < 8){
+            passwordError.setVisible(true);
+            passwordError.setText("Password must contain at least 8 characters");
+            valid = false;
+        }
+        else {
+            passwordError.setVisible(false);
+        }
+
+        if(valid){
+            // TODO: Call signup() from backend
+        }
+
     }
 
     @FXML
